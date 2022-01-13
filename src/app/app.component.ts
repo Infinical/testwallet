80c8d7339c80b5c855a20f81725dea61c8ead4db
Infinical <inficalvin@gmail.com>
2022-01-13 07:48:47 +0300

    Add wallet connect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import Web3 from "web3";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import { DOCUMENT } from '@angular/common';
 import MetaMaskOnboarding from '@metamask/onboarding';
+import WalletConnect from "@walletconnect/client";
+import QRCodeModal from "@walletconnect/qrcode-modal";
 
 @Component({
   selector: 'app-root',
@@ -16,9 +18,16 @@ export class AppComponent implements OnInit {
 
 
   private window: any
+  private connector: any
 
   constructor(@Inject(DOCUMENT) private document: Document) {
     this.window = this.document.defaultView;
+
+    // Create a connector
+    this.connector = new WalletConnect({
+      bridge: "https://bridge.walletconnect.org", // Required
+      qrcodeModal: QRCodeModal,
+    });
   }
 
   ngOnInit() {
@@ -74,29 +83,65 @@ export class AppComponent implements OnInit {
 
 
 
-  async testModal() {
 
 
 
-    // const providerOptions = {
-    //   walletconnect: {
-    //     package: WalletConnectProvider, // required
-    //     options: {
-    //       infuraId: "5b62013b0c764aa0a0f58a590e6ce464" // required
-    //     }
-    //   }
-    // };
+  initializeWalletConnect() {
+
+    if (!this.connector.connected) {
+
+      this.connector.createSession();
+    }
+
+    this.connector.on("connect", (error: any, payload: any) => {
+      if (error) {
+        throw error;
+      }
+      const { accounts, chainId } = payload.params[0];
+    });
+
+    this.connector.on("session_update", (error: any, payload: any) => {
+      if (error) {
+        throw error;
+      }
+
+      // Get updated accounts and chainId
+      const { accounts, chainId } = payload.params[0];
+    });
+
+    this.connector.on("disconnect", (error: any, payload: any) => {
+      if (error) {
+        throw error;
+      }
+
+      // Delete connector
+    });
+  }
 
 
-    // // const web3Modal = new Web3Modal({
-    // //   network: "mainnet", // optional
-    // //   cacheProvider: true, // optional
-    // //   providerOptions // required
-    // // });
+  sendWalletCOnnect() {
 
-    // const provider = await web3Modal.connect();
+    const tx = {
+      from: "0xbc28Ea04101F03aA7a94C1379bc3AB32E65e62d3", // Required
+      to: "0x89D24A7b4cCB1b6fAA2625Fe562bDd9A23260359", // Required (for non contract deployments)
+      data: "0x", // Required
+      gasPrice: "0x02540be400", // Optional
+      gas: "0x9c40", // Optional
+      value: "0x00", // Optional
+      nonce: "0x0114", // Optional
+    };
 
-    // const web3 = new Web3(provider);
+    // Send transaction
+    this.connector
+      .sendTransaction(tx)
+      .then((result: any) => {
+
+        console.log(result);
+      })
+      .catch((error: any) => {
+
+        console.error(error);
+      });
   }
 
 }
